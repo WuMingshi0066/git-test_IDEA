73616fcf8c9f808ba9552aea730fa28dba0645ab
Form <Form@myPC.com>
2022-03-22 01:09:08 +0800

    first commit

---
+++ src/main/java/com/sgg/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.sgg.git;
+
+import org.junit.Test;
+
+/**
+ * author: NoName
+ * create: 2022-03-22 00:54
+ */
+public class GitTest {
+
+    @Test
+    public void test1() {
+        System.out.println("hello git in IDEA !!");
+    }
+
+}
